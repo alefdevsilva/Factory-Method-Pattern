cc0eb148af15bc2ab64b336103a15840cdea6966
Alef <alef.dev.silva@gmail.com>
2022-04-08 16:09:36 -0300

    Criando a classe Creator

---
+++ Creator/CartaoFactory.cs
@@ -0,0 +1,11 @@
+using Factory_Method_Pattern.Product;
+
+namespace Factory_Method_Pattern.Creator
+{
+    public abstract class CartaoFactory
+    {
+        public abstract CartaoCredito BuscarCartaoCredito();
+
+        
+    }
+}
